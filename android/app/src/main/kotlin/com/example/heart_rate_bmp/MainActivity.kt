e97a91ba0de18246893f6b15e48e9fb89331900f
fatmatmakram <rawdaelkamnoury@std.mans.edu.eg>
2024-10-17 12:26:39 +0300

    set up watch ios

---
+++ android/app/src/main/kotlin/com/example/heart_rate_bmp/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.heart_rate_bmp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ ios/watch_heart Watch App/watch_heartApp.swift
@@ -0,0 +1,17 @@
+//
+//  watch_heartApp.swift
+//  watch_heart Watch App
+//
+//  Created by megamind fatma on 16/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct watch_heart_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
